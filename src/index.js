2da60177a2bfb47e3000a27cc1b3fe9283bb4f76
dev-ths <alphavo.ths@gmail.com>
2024-02-27 16:08:21 -0800

    enabled redux-toolkit & disabled blocking code in order to make it work

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react"
+import { render } from "react-dom"
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+// import { PersistGate } from "redux-persist/integration/react"
 
-import App from './App';
-import { store, persistor } from './store/store';
+import App from "./App"
 
-import './index.scss';
+// import { store, persistor } from "./store/store"
+import { store } from "./store/store"
 
-const rootElement = document.getElementById('root');
+import "./index.scss"
+
+const rootElement = document.getElementById("root")
 
 render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>,
-  rootElement
-);
+	<React.StrictMode>
+		<Provider store={store}>
+			{/* <PersistGate loading={null} persistor={persistor}> /\/\ disabled for redux-toolkit */}
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+			{/* </PersistGate> /\/\ disabled for redux-toolkit */}
+		</Provider>
+	</React.StrictMode>,
+	rootElement,
+)
--- src/store/store.js
+++ src/store/store.js
@@ -1,34 +1,44 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import logger from 'redux-logger';
+// import { compose, createStore, applyMiddleware } from "redux"
+import { configureStore } from "@reduxjs/toolkit"
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+import logger from "redux-logger"
 
-import { rootReducer } from './root-reducer';
+import { rootReducer } from "./root-reducer"
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-  Boolean
-);
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(Boolean)
 
-const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+// const composeEnhancer =
+//   (process.env.NODE_ENV !== 'production' &&
+//     window &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['user'],
-};
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   blacklist: ['user'],
+// };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
-export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
-);
+// export const store = createStore(
+//   persistedReducer,
+//   undefined,
+//   composedEnhancers
+// );
 
-export const persistor = persistStore(store);
+//--------------------------------------------------------------------------------
+export const store = configureStore({
+	reducer: rootReducer,
+	// middleware: middleWares, // redux-thunk -> promise based actions -> used by default if no middleware options are provided -> also adds 2 other middlewares by default
+})
+// export const store = createStore(
+//   persistedReducer,
+//   undefined,
+//   composedEnhancers
+// );
+
+// export const persistor = persistStore(store);
